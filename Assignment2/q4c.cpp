e004768c82ad9096c564dc6389dedabef9ad040c
grey289 <shivanshsahu982@gmail.com>
2025-08-28 02:05:45 +0530

    Update q4c.cpp

---
+++ Assignment2/q4c.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+int main()
+{
+    cout<<"enter any word"<<endl;
+    string str;
+    cin>>str;
+    cout<<"deleting vowels"<<endl;
+    
+    return 0; 
+}

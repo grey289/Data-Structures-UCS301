dda74630af1e00d5ec52e0d2cf9c405d4e009e8e
grey289 <shivanshsahu982@gmail.com>
2025-08-28 01:05:34 +0530

    Update q3a.cpp

--- Assignment2/q3a.cpp
+++ Assignment2/q3a.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 using namespace std;
 
+int missing(int *array,int s){
+    if (array[0]!=1){
+        return array[0];
+    }
+    int i=1;
+    for (i=1;i<s;++i){
+    if (array[i]!=i+1) {
+
+        return i+1;}
+    }
+    return -1;
+}
+
 int main()
 {
    int elements,i=0,j,temp;
@@ -12,31 +25,17 @@ int main()
         cout<<"enter the "<<i+1<<" element"<<endl;
 cin>> arr[i];
     }
+    int s=sizeof(arr)/sizeof(arr[0]);
     //sorting array 
-   
-    for (i=0;i<elements;i++){
-        for (j=0;j<elements-i-1;j++){
-            if (arr[j]>arr[j+1]){
-                temp=arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=temp;
-            }
-        }
-    }
+    int num=missing(arr,s);
+    cout <<"for the array ;-"<<endl;
     for (i=0;i<elements;i++){
         cout<<arr[i]<<"\t";
     }
+    if (num==-1){
+        cout<<endl<<"there is no missing number in array"<<endl;
+        
+    } else cout<<endl<<"the missing number is "<<num<<endl;
 
-    //sorted array of index till n can have distict numbers from 1 to n 
-    int missing =0,num;
-    for (i=0;i<elements-1;i++){
-        if (arr[i+1]==arr[i]+1)
-        continue;
-        else {num=arr[i]+1;
-            missing =1;}
-    }
-
-    if (missing==1) cout<<"the missing element is "<<num<<endl;
-    else cout<<"no missing numbers in array"<<endl;
     return 0; 
 }

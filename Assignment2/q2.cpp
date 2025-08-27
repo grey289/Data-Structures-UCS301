296651615f6e36531e52e133d6df30c44c374a0d
grey289 <shivanshsahu982@gmail.com>
2025-08-28 01:05:29 +0530

    Update q2.cpp

--- Assignment2/q2.cpp
+++ Assignment2/q2.cpp
@@ -1,25 +1,28 @@
 #include <iostream>
-#include <iomanip>
-#include <string>
 using namespace std;
-
-int main()
-{
-    int arr[]={64,34,25,12,22,11,90},i=0,j,temp;
-    int size=sizeof(arr)/sizeof(arr[0]);
-    for (i=0;i<size;i++){
-        for (j=0;j<size-i-1;j++){
-            if (arr[j]>arr[j+1]){
+void bubblesort(int arr[],int size){
+    int i=0,j,temp;
+    for(;i<size;i++){
+        bool swap=false;
+        for(j=0;j<size-i-1;j++){
+            if(arr[j]>arr[j+1]){
                 temp=arr[j];
                 arr[j]=arr[j+1];
                 arr[j+1]=temp;
+                swap=true;
             }
         }
+        if(swap==false) return;
     }
-    for (i=0;i<size;i++){
+    return;
+}
+int main()
+{
+    int arr[]={64,34,25,12,22,11,90},i=0;
+    int size=sizeof(arr)/sizeof(arr[0]);
+    bubblesort(arr,size);
+    for (;i<size;i++){
         cout<<arr[i]<<"\t";
     }
-
-
     return 0; 
 }

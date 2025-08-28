b8a4d8271e6a9393f2f6a96b54e3b7ab02fc4bcd
grey289 <shivanshsahu982@gmail.com>
2025-08-28 14:53:25 +0530

    Update q5c.cpp

--- Assignment2/q5c.cpp
+++ Assignment2/q5c.cpp
@@ -5,13 +5,28 @@ int main()
 {
     cout<<"enter the order of which you want a matrix"<<endl;
     int r;
-    cin>>r;int lenght=(r*(r-1))/2;
+    cin>>r;
+    int lenght=(r*(r-1))/2;
     int arr[lenght];
-    for(int i=0;i<lenght;i++){
-        if (i<r-1){
-        cout<<"enter element of index ( "<<i+1<<","<<i<<")"<<endl;
-        cin>>arr[i];
-        } //else if 
+    int index = 0;
+for (int i = 0; i < r; i++) {
+    for (int j = 0; j < i; j++) {  
+        cout<<"enter the ("<<i<<","<<j<<")"<<endl;
+        cin >> arr[index];
+        index++;
     }
+}
+index =0;
+
+cout<<"the entered matrix is :-"<<endl;
+for(int i=0;i<r;i++){
+    for(int j=0;j<r;j++){
+        if(j<i){
+            cout<<arr[index]<<"\t";
+            index++;
+        } else cout<<"0\t";
+    }
+    cout<<endl;
+}
     return 0; 
 }

850eeb76acc00226b00d5edc8c15f691b32fc1d6
grey289 <shivanshsahu982@gmail.com>
2025-08-28 14:53:30 +0530

    Update q4c.cpp

--- Assignment2/q4c.cpp
+++ Assignment2/q4c.cpp
@@ -1,14 +1,31 @@
 #include <iostream>
-#include <iomanip>
 #include <string>
 using namespace std;
 
-int main()
-{
-    cout<<"enter any word"<<endl;
-    string str;
-    cin>>str;
-    cout<<"deleting vowels"<<endl;
+string removeVowels(string str) {
+    string result = "";
     
-    return 0; 
+    for (int i = 0; i < str.length(); i++) {
+        char ch = str[i];
+        // Check if character is not a vowel (both lowercase and uppercase)
+        if (ch != 'a' && ch != 'e' && ch != 'i' && ch != 'o' && ch != 'u' &&
+            ch != 'A' && ch != 'E' && ch != 'I' && ch != 'O' && ch != 'U') {
+            result += ch;
+        }
+    }
+    
+    return result;
+}
+
+int main() {
+    string input;
+    
+    cout << "Enter a string: ";
+    getline(cin, input);
+    
+    string output = removeVowels(input);
+    
+    cout << "String after removing vowels: " << output << endl;
+    
+    return 0;
 }

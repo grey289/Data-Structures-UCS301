6f56187a10c1b591da3cc480b8b88217e613861d
grey289 <shivanshsahu982@gmail.com>
2025-09-18 18:42:19 +0530

    Update q1additional.cpp

--- Assignment 3/q1additional.cpp
+++ Assignment 3/q1additional.cpp
@@ -33,3 +33,29 @@ int main() {
     for(int i=0; i<n; i++)
         cout << ans[i] << " ";
 }
+
+/*
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    int arr[] = {4, 5, 2, 10, 8};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int ans[n];
+    Stack<int> s;
+
+    for(int i=n-1; i>=0; i--) {
+        while(!s.empty() && s.peek() =< arr[i])
+            s.pop();
+
+        if(s.empty()) ans[i] = 0;
+        else ans[i] = arr.find(s.peek())-i;
+
+        s.push(arr[i]);
+    }
+
+    return ans;
+}
+
+*/
